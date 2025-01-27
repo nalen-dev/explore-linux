76de6f32b6ecc72167e0fe8777ab0ce355a8b0ee
nalendev <nalendrapraja11@gmail.com>
2025-01-28 07:27:18 +0800

    simple copy prog

--- lib/error_functions.c
+++ lib/error_functions.c
@@ -14,7 +14,7 @@ NORETURN static void terminate(Boolean useExit3){
 
    s = getenv("EF_DUMPCORE");
 
-   if (s != NULL && *s != '\o')
+   if (s != NULL && *s != '\0')
         abort();
    else if(useExit3)
         exit(EXIT_FAILURE);
@@ -22,28 +22,34 @@ NORETURN static void terminate(Boolean useExit3){
         _exit(EXIT_FAILURE);
 }
 
-static void outputError(Boolean useErr, int err, Boolean flushStdout, 
-                const char *format, va_list ap)
+static void outputError(Boolean useErr, int err, Boolean flushStdout,
+        const char *format, va_list ap)
 {
-    #define BUF_SIZE 500
-    
+#define BUF_SIZE 500
     char buf[BUF_SIZE], userMsg[BUF_SIZE], errText[BUF_SIZE];
 
     vsnprintf(userMsg, BUF_SIZE, format, ap);
 
     if (useErr)
-        snprintf(errText, BUF_SIZE, " [%s %s]", 
-                (err > 0 && err <= MAX_ENAME) ?  
+        snprintf(errText, BUF_SIZE, " [%s %s]",
+                (err > 0 && err <= MAX_ENAME) ?
                 ename[err] : "?UNKNOWN?", strerror(err));
     else
         snprintf(errText, BUF_SIZE, ":");
 
+#if __GNUC__ >= 7
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wformat-truncation"
+#endif
     snprintf(buf, BUF_SIZE, "ERROR%s %s\n", errText, userMsg);
+#if __GNUC__ >= 7
+#pragma GCC diagnostic pop
+#endif
 
-    if(flushStdout)
-        fflush(stdout);
+    if (flushStdout)
+        fflush(stdout);       /* Flush any pending stdout */
     fputs(buf, stderr);
-    fflush(stderr);              
+    fflush(stderr);           /* In case stderr is not line-buffered */
 }
 
 void errMsg(const char *format, ...)
